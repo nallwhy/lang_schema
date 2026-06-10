7d0c8bc120f544bd644601367726797af37d61b0
Jinkyou Son <nallwhy@gmail.com>
2026-06-10 14:08:41 +0900

    Bump deps

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule LangSchema.MixProject do
     [
       {:recase, "~> 0.8.1"},
       {:jason, "~> 1.3"},
-      {:langchain, "~> 0.6.1", optional: true},
+      {:ash, "~> 3.0", optional: true},
+      {:langchain, ">= 0.6.1", optional: true},
       {:ex_doc, "~> 0.34", only: :doc, runtime: false}
     ]
   end
